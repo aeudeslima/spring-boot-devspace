625f029585c6b7933cdffa243df8a73d3040df48
Eudes <aeudes@gmail.com>
2018-08-30 15:17:52 -0300

    first commit

---
+++ src/main/java/com/example/springbootdevspace/SpringBootDevspaceApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootdevspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevspaceApplication.class, args);
+	}
+}
