bfd0cb3289d5042646aa3fc4a8936213be40616c
Eudes <aeudes@gmail.com>
2018-08-31 08:37:39 -0300

    Included simple hello response

--- src/main/java/com/example/springbootdevspace/SpringBootDevspaceApplication.java
+++ src/main/java/com/example/springbootdevspace/SpringBootDevspaceApplication.java
@@ -1,12 +1,16 @@
 package com.example.springbootdevspace;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringBootDevspaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDevspaceApplication.class, args);
 	}
+	
 }
